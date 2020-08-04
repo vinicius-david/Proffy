8feed6fb4fce739c5e1c80662aeed51dafd86115
Vinicius David <vinicius.davidf@gmail.com>
2020-08-04 15:13:07 -0300

    landing page

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
+
+function App(): JSX.Element {
+  return <Landing />;
+}
+
+export default App;
